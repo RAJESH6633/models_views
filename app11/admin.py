ee2681433d9f6fd87a665d2ba7c2a6a10e756a60
B.S RAJESH <123260548+RAJESH6633@users.noreply.github.com>
2023-06-16 13:51:36 +0530

    first commit

---
+++ app11/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from app11.models import *
+# Register your models here.
+
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecords)
